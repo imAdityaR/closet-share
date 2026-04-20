69dfa60a6adf06df11491882ab619743d791e64e
imAdityaR <161571694+imAdityaR@users.noreply.github.com>
2026-04-20 10:35:22 +0530

    fix: Added Vercel  Web-Analytics and Speed_Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Cormorant_Garamond, Inter } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
+import { Analytics, } from '@vercel/analytics/next'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { CartProvider } from "@/lib/cart-context"
@@ -8,6 +9,7 @@ import { WishlistProvider } from "@/lib/wishlist-context"
 import { RootLayoutClient } from '@/components/root-layout-client'
 import { Toast } from '@radix-ui/react-toast'
 import { ToastProvider } from '@/lib/toast-context'
+
 
 const cormorant = Cormorant_Garamond({
   subsets: ['latin'],
